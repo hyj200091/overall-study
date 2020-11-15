19b46f43d66786b255d6e2daca0f629c1be42ab7
侯粤嘉 <2298090131@qq.com>
2020-11-15 23:04:42 +0800

    完善发送短信接口

--- soft-news/soft-news-service-user/src/main/java/com/soft1851/user/controller/UserController.java
+++ soft-news/soft-news-service-user/src/main/java/com/soft1851/user/controller/UserController.java
@@ -4,12 +4,17 @@ import com.soft1851.api.controller.user.UserControllerApi;
 import com.soft1851.pojo.AppUser;
 import com.soft1851.result.GraceResult;
 import com.soft1851.user.mapper.AppUserMapper;
+import com.soft1851.utils.IpUtil;
+import com.soft1851.utils.RedisOperator;
 import com.soft1851.utils.SmsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
+import static com.soft1851.api.BaseController.MOBILE_SMSCODE;
 
 /**
  * @ClassName UserController
@@ -19,6 +24,8 @@ import javax.servlet.http.HttpServletRequest;
  **/
 @RestController
 public class UserController implements UserControllerApi {
+    @Autowired
+    private RedisOperator redis;
     @Resource
     private AppUserMapper appUserMapper;
     @Resource
@@ -41,9 +48,18 @@ public class UserController implements UserControllerApi {
     @Override
     public GraceResult getCode(String mobile, HttpServletRequest request) {
         // 生成随机验证码并且发送短信
-        String random = (int) ((Math.random() * 9 + 1) * 10000) + "";
-        System.out.println(random);
-        smsUtil.sendSms(mobile,random);
+//        String random = (int) ((Math.random() * 9 + 1) * 10000) + "";
+//        System.out.println(random);
+//        smsUtil.sendSms(mobile,random);
+        // 获得用户id
+        String userIp = IpUtil.getRequestIp(request);
+        // 根据用户的ip进行限制，限制用户在60秒只能获得一次验证码
+        redis.setnx60s(MOBILE_SMSCODE + ":" + userIp,userIp);
+        // 生成随机验证码并且发送验证码
+         String random = (int) ((Math.random() * 9 + 1) * 100000) + "";
+         smsUtil.sendSms(mobile,random);
+         // 把验证码存入redis，用户后续进行验证
+        redis.set(MOBILE_SMSCODE + ":" + mobile,random, 30 * 60);
         return GraceResult.ok();
     }
 }
